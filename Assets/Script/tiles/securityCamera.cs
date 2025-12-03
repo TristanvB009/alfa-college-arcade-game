d1cb0305cb47ea0b797ef8adef2cc4752c6b6452
okranixkidneygravity <191375715+okranixkidneygravity@users.noreply.github.com>
2025-12-03 13:24:43 +0100

    collider-based security camera finished

--- Assets/Script/tiles/securityCamera.cs
+++ Assets/Script/tiles/securityCamera.cs
@@ -1,14 +1,31 @@
-using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
 public class SecurityCamera : MonoBehaviour
 {
     public int activationInterval;
     public bool isPowered;
-    private bool isActivated;
+    [SerializeField]private bool isActivated;
 
-    public void OnPlayerEnteredVision()
+    private bool isRunningActivationCycle;
+    
+    private void Update()
     {
-        
+        if (isPowered && activationInterval > 0 && !isRunningActivationCycle)
+        {
+            StartCoroutine(ActivationCycle());
+        }
+    }
+
+    IEnumerator ActivationCycle()
+    {
+        isRunningActivationCycle = true;
+        isActivated = true;
+        Debug.Log("Camera Activated");
+        yield return new WaitForSeconds(activationInterval);
+        isActivated = false;
+        Debug.Log("Camera Deactivated");
+        yield return new WaitForSeconds(activationInterval);
+        isRunningActivationCycle = false;
     }
 }
--- Assets/Script/tiles/securityCameraVision.cs
+++ Assets/Script/tiles/securityCameraVision.cs
@@ -1,12 +1,42 @@
-using System.Collections.Generic;
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class securityCameraVision : MonoBehaviour
 {
     public SecurityCamera securityCamera;
     public string playerTag = "Player";
+    public float rotationSpeed;
+    public int rotationPause;
+    public float rotationAngleStart;
+    public float rotationAngleEnd;
+    private bool isRotationPaused;
+
+    private float targetAngle;
 
     private bool playerDetected;
+
+    private void Start()
+    {
+        targetAngle = rotationAngleEnd;
+        playerDetected = false;
+        isRotationPaused = false;
+    } 
+
+    private void Update()
+    {
+        if (!isRotationPaused)
+        {
+            float step = rotationSpeed * Time.deltaTime;
+            transform.Rotate(0, 0, step);
+
+            if (Mathf.Abs(Mathf.DeltaAngle(transform.eulerAngles.z, targetAngle)) < Math.Abs(step) + 0.1f)
+            {
+                Debug.Log("Reached target rotation");
+                StartCoroutine(PauseAndSwitchTarget());
+            }   
+        }
+    }
 
     private void OnTriggerEnter2D(Collider2D vision)
     {
@@ -25,8 +55,22 @@ public class securityCameraVision : MonoBehaviour
             Debug.Log("player out of vision");
        }
     }
+
+    IEnumerator PauseAndSwitchTarget()
+    {
+        isRotationPaused = true;
+            yield return new WaitForSeconds(rotationPause);
+            isRotationPaused = false;
+            if (Mathf.Approximately(targetAngle, rotationAngleEnd))
+            {
+                targetAngle = rotationAngleStart;
+                rotationSpeed = -rotationSpeed;
+            }
+            else
+            {
+                targetAngle = rotationAngleEnd;
+                rotationSpeed = -rotationSpeed;
+            }
+            Debug.Log("New target angle: " + targetAngle);
+    }
 }
-// TODO
-// - Improve Player Tag getting
-// - Activation Timer
-// - Configurable Camera Rotation
